be604461982c92bf3d335b90c08aa8218dc49548
aiko030500-wq <aiko030500@gmail.com>
2025-12-15 02:41:50 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,109 @@
+// data.js — AI BAYAN VISUAL DICTIONARY 4 GRADE
+// 8 Modules × 8 Lessons
+// Words EN + RU + image + sound
+// Logo: logo.png (already exists)
+
+window.APP_DATA = {
+  modules: [
+    {
+      id: "m1",
+      title: "Module 1 — All About Me",
+      color: "violet",
+      lessons: [
+        {
+          id: "m1l1",
+          title: "Daily routines",
+          words: [
+            { en: "get up", ru: "вставать", img: "img/get_up.png" },
+            { en: "wash my face", ru: "умываться", img: "img/wash_face.png" },
+            { en: "brush my teeth", ru: "чистить зубы", img: "img/brush_teeth.png" },
+            { en: "have breakfast", ru: "завтракать", img: "img/breakfast.png" },
+            { en: "go to school", ru: "идти в школу", img: "img/go_school.png" },
+            { en: "do homework", ru: "делать уроки", img: "img/homework.png" },
+            { en: "go to bed", ru: "ложиться спать", img: "img/bed.png" }
+          ]
+        },
+        {
+          id: "m1l2",
+          title: "My family",
+          words: [
+            { en: "mother", ru: "мама", img: "img/mother.png" },
+            { en: "father", ru: "папа", img: "img/father.png" },
+            { en: "sister", ru: "сестра", img: "img/sister.png" },
+            { en: "brother", ru: "брат", img: "img/brother.png" },
+            { en: "parents", ru: "родители", img: "img/parents.png" },
+            { en: "grandmother", ru: "бабушка", img: "img/grandmother.png" },
+            { en: "grandfather", ru: "дедушка", img: "img/grandfather.png" }
+          ]
+        },
+        {
+          id: "m1l3",
+          title: "My home",
+          words: [
+            { en: "house", ru: "дом", img: "img/house.png" },
+            { en: "flat", ru: "квартира", img: "img/flat.png" },
+            { en: "room", ru: "комната", img: "img/room.png" },
+            { en: "kitchen", ru: "кухня", img: "img/kitchen.png" },
+            { en: "bedroom", ru: "спальня", img: "img/bedroom.png" },
+            { en: "bathroom", ru: "ванная", img: "img/bathroom.png" }
+          ]
+        },
+        {
+          id: "m1l4",
+          title: "Free time",
+          words: [
+            { en: "play games", ru: "играть", img: "img/play.png" },
+            { en: "watch TV", ru: "смотреть ТВ", img: "img/tv.png" },
+            { en: "read books", ru: "читать книги", img: "img/read.png" },
+            { en: "listen to music", ru: "слушать музыку", img: "img/music.png" },
+            { en: "draw", ru: "рисовать", img: "img/draw.png" },
+            { en: "ride a bike", ru: "кататься на велосипеде", img: "img/bike.png" }
+          ]
+        },
+        {
+          id: "m1l5",
+          title: "Feelings",
+          words: [
+            { en: "happy", ru: "счастливый", img: "img/happy.png" },
+            { en: "sad", ru: "грустный", img: "img/sad.png" },
+            { en: "angry", ru: "сердитый", img: "img/angry.png" },
+            { en: "tired", ru: "уставший", img: "img/tired.png" },
+            { en: "excited", ru: "взволнованный", img: "img/excited.png" }
+          ]
+        },
+        {
+          id: "m1l6",
+          title: "Friends",
+          words: [
+            { en: "friend", ru: "друг", img: "img/friend.png" },
+            { en: "help", ru: "помогать", img: "img/help.png" },
+            { en: "share", ru: "делиться", img: "img/share.png" },
+            { en: "play together", ru: "играть вместе", img: "img/together.png" }
+          ]
+        },
+        {
+          id: "m1l7",
+          title: "School day",
+          words: [
+            { en: "lesson", ru: "урок", img: "img/lesson.png" },
+            { en: "break", ru: "перемена", img: "img/break.png" },
+            { en: "teacher", ru: "учитель", img: "img/teacher.png" },
+            { en: "pupil", ru: "ученик", img: "img/pupil.png" },
+            { en: "classroom", ru: "класс", img: "img/classroom.png" }
+          ]
+        },
+        {
+          id: "m1l8",
+          title: "Review",
+          words: [
+            { en: "family", ru: "семья", img: "img/family.png" },
+            { en: "school", ru: "школа", img: "img/school.png" },
+            { en: "home", ru: "дом", img: "img/home.png" }
+          ]
+        }
+      ]
+    }
+
+    // 👉 Modules 2–8 продолжаются по ТОЙ ЖЕ СХЕМЕ
+  ]
+};
